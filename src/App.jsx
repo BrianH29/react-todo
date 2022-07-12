6ff764de742f8bd8214ce9b14107b321d192d3e9
BrianH <brianhwang29@gmail.com>
2022-07-13 08:45:16 +0900

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
 import Header from './components/Header';
-import inputTodo from "./components/InputTodo";
 import InputTodo from "./components/InputTodo";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <Header />
--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,7 +1,23 @@
-function inputTodo(){
+import '../css/InputTodo.css'
+import {useState} from 'react';
+
+function InputTodo(){
+    const [todo, setTodo] = useState('');
+    const [todos, setTodos] = useState([]);
+    const onChange = (e) => setTodo(e.target.value);
+    const onSubmit = (e) =>  {
+        e.preventDefault();
+        if(todo === '') return;
+        setTodo('');
+        setTodos(curr => [todo, ...curr])
+    }
+
     return(
-        <input type="text" placeholder="What are you up to?" />
+        <form className="input-todo d-flex d-center" onSubmit={onSubmit}>
+            <input type="text" placeholder="What are you up to?" onChange={onChange} value={todo}/>
+            <button>todo</button>
+        </form>
     )
 }
 
-export default inputTodo;
+export default InputTodo;
