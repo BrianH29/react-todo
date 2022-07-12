765773e814beb05e76bff1347a7e5d518b28cf46
Brian Hwang <brianhwang29@gmail.com>
2022-07-13 00:10:09 +0900

    modified

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import '../css/Header.css'
+import '../css/main.css'
+
+function Header(){
+    return (
+        <header className="d-flex d-center todo-header">Today's ToDo!</header>
+    )
+}
+
+export default Header
---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,7 @@
+function inputTodo(){
+    return(
+        <input type="text" placeholder="What are you up to?" />
+    )
+}
+
+export default inputTodo;
