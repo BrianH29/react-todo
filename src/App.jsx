7ae803dc3614d0f03d0fad2dcdb2e374a8b10977
BrianH <brianhwang29@gmail.com>
2022-07-18 08:49:55 +0900

    added remove todo

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,21 @@ import './css/reset.css'
 function App() {
     const [todoList, setTodoList] = useState([]);
     const handleClick = (todo) => setTodoList(curr => [todo, ...curr]);
+    const deleteTodo = (todo) => {
+        console.log('app.jsx todod : ', todo)
+        const refreshList = todoList.filter(item => item !== todo);
+        setTodoList(refreshList)
+
+    }
     useEffect(() => {
-        console.log('todoList : ', todoList);
-        localStorage.setItem('todos', JSON.stringify(todoList))
+        localStorage.setItem('todos', JSON.stringify(todoList));
     },[todoList])
 
   return (
     <div>
       <Header />
         <InputTodo childPropFn={handleClick}/>
-        <ListTodo todoList={todoList}/>
+        <ListTodo todoList={todoList} deleteTodo={deleteTodo}/>
     </div>
   )
 }
--- src/components/ListTodo.jsx
+++ src/components/ListTodo.jsx
@@ -1,16 +1,25 @@
 import '../css/ListTodo.css';
 
-export default function ListTodo({todoList}){
-    const onClick = (e) => {
-        console.log('its on click ', e)
+export default function ListTodo({todoList, deleteTodo}){
+    const onClick = (e, sort, item) => {
+       switch(sort){
+           case 'delete' :
+               deleteTodo(item);
+               return;
+       }
     }
+
     return (
-        <ul>
+        <ul className="list-container">
             {
                 todoList.map((item, idx) =>
-                    <li key={idx} className="checkbox-circle">
-                        <input type="checkbox" id={item} value={item} onClick={onClick}/>
-                        <label htmlFor={item}>{item}</label>
+                    <li key={idx} className="checkbox-circle d-flex d-between">
+                        <div>
+                            <input type="checkbox" id={item} value={item}/>
+                            <label htmlFor={item}>{item}</label>
+                        </div>
+                        <div onClick={(e) => onClick(e, 'delete', item)}>❌</div>
+                        {/*<div onClick={this.onClick.bind(this, 'delete', idx)}>❌</div>*/}
                     </li> )
             }
         </ul>
